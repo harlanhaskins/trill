aa49d7651740897ff1a8da6f39e84356738bf79e
Harlan Haskins <harlan@harlanhaskins.com>
2017-03-02 13:18:12 -0500

    Replaced backtick code voice with `\c` code voice, for Xcode.

--- Sources/Runtime/metadata.h
+++ Sources/Runtime/metadata.h
@@ -22,8 +22,8 @@ extern "C" {
 
 
 /**
- `TRILL_ANY` is a special type understood by the Trill compiler as the
- representation of an `Any` value.
+ \c TRILL_ANY is a special type understood by the Trill compiler as the
+ representation of an \c Any value.
  */
 typedef struct TRILL_ANY {
   void * _Nonnull _any;
@@ -76,11 +76,11 @@ uint64_t trill_getNumFields(const void *_Nonnull typeMeta);
 
 
 /**
- Gets the `FieldMetadata` associated with the provided field index into the
- provided `TypeMetadata`.
+ Gets the \c FieldMetadata associated with the provided field index into the
+ provided \c TypeMetadata.
  
  @note This function will abort if the field index is out of bounds. Ensure
-       the field you pass in is in-bounds by calling `trill_getNumFields` and
+       the field you pass in is in-bounds by calling \c trill_getNumFields and
        comparing the result.
 
  @param typeMeta The type metadata.
@@ -91,7 +91,7 @@ const void *_Nonnull trill_getFieldMetadata(const void *_Nonnull typeMeta,
 
 
 /**
- Gets the name of the provided `FieldMetadata`.
+ Gets the name of the provided \c FieldMetadata.
 
  @param fieldMeta The field metadata.
  @return A constant C string with the field's name as declared in the source.
@@ -100,7 +100,7 @@ const char *_Nonnull trill_getFieldName(const void *_Nonnull fieldMeta);
 
 
 /**
- Gets the `TypeMetadata` of the provided `FieldMetadata`.
+ Gets the \c TypeMetadata of the provided \c FieldMetadata.
 
  @param fieldMeta The field metadata.
  @return The metadata of the field's type.
@@ -118,44 +118,44 @@ size_t trill_getFieldOffset(const void *_Nullable fieldMeta);
 
 
 /**
- Creates an `Any` representation with the provided type metadata.
- An `Any` in Trill is a variable-sized, heap-allocated box that holds:
+ Creates an \c Any representation with the provided type metadata.
+ An \c Any in Trill is a variable-sized, heap-allocated box that holds:
 
  - Type metadata for the underlying object, and
  - A payload that is the size specified in the metadata.
  
  @note This value is uninitialized, and the payload will be empty. You
        must initialize the payload with a value by casting and storing
-       the value into the pointer returned by `trill_getAnyValuePtr`.
+       the value into the pointer returned by \c trill_getAnyValuePtr.
 
  @param typeMeta The type metadata for the underlying value.
- @return A new `Any` box that is uninitialized.
+ @return A new \c Any box that is uninitialized.
  */
 TRILL_ANY trill_allocateAny(const void *_Nonnull typeMeta);
 
 
 /**
- Copies an `Any` if the underlying value's semantics mean it should be copied.
- If the underlying value is a reference type, then the provided `Any` is just
+ Copies an \c Any if the underlying value's semantics mean it should be copied.
+ If the underlying value is a reference type, then the provided \c Any is just
  returned unmodified.
 
- @param any The `Any` you wish to copy.
- @return A new `Any` containing the contents of the old `Any`, if the
+ @param any The \c Any you wish to copy.
+ @return A new \c Any containing the contents of the old \c Any, if the
          underlying value is has value semantics. Otherwise, the provided
-         `Any`.
+         \c Any.
  */
 TRILL_ANY trill_copyAny(TRILL_ANY any);
 
 /**
- Gets a pointer to a field inside the `Any` structure. Specifically, this
+ Gets a pointer to a field inside the \c Any structure. Specifically, this
  is a pointer inside the payload that will, when stored, update the value
  inside the payload.
 
  @note This function will abort if the field index is out of bounds. Ensure
-       the field you pass in is in-bounds by calling `trill_getNumFields` and
+       the field you pass in is in-bounds by calling \c trill_getNumFields and
        comparing the result.
 
- @param any_ The `Any` you're inspecting.
+ @param any_ The \c Any you're inspecting.
  @param fieldNum The field index you're accessing.
  @return A pointer into the payload that points to the value of the
          provided field.
@@ -164,26 +164,26 @@ void *_Nonnull trill_getAnyFieldValuePtr(TRILL_ANY any_, uint64_t fieldNum);
 
 
 /**
- Extracts a field from this payload and wraps it in its own `Any` container.
+ Extracts a field from this payload and wraps it in its own \c Any container.
  
 
  @note This function will abort if the field index is out of bounds. Ensure
-       the field you pass in is in-bounds by calling `trill_getNumFields` and
+       the field you pass in is in-bounds by calling \c trill_getNumFields and
        comparing the result.
 
  @param any_ The composite type from which you're extracting a field.
  @param fieldNum The field index.
- @return A new `Any` with a payload that comes from the field's contents.
+ @return A new \c Any with a payload that comes from the field's contents.
  */
 TRILL_ANY trill_extractAnyField(TRILL_ANY any_, uint64_t fieldNum);
 
 
 /**
- Updates a field with the value inside the provided `Any`.
+ Updates a field with the value inside the provided \c Any.
 
- @param any_ The `Any` for the composite type whose field you are replacing.
+ @param any_ The \c Any for the composite type whose field you are replacing.
  @param fieldNum The index of the field to be replaced.
- @param newAny_ The `Any` for the underlying field.
+ @param newAny_ The \c Any for the underlying field.
  */
 void trill_updateAny(TRILL_ANY any_, uint64_t fieldNum, TRILL_ANY newAny_);
 
@@ -194,43 +194,43 @@ void trill_updateAny(TRILL_ANY any_, uint64_t fieldNum, TRILL_ANY newAny_);
  @note This will perform no casting or type checking for you, and should only
        be used opaquely or if you are absolutely sure of the underlying type.
 
- @param anyValue The `Any` whose payload you want to use.
+ @param anyValue The \c Any whose payload you want to use.
  @return A pointer to the payload that can be cast and then loaded from.
  */
 void *_Nonnull trill_getAnyValuePtr(TRILL_ANY anyValue);
 
 
 /**
- Gets the `TypeMetadata` underlying an `Any` box.
+ Gets the \c TypeMetadata underlying an \c Any box.
 
- @param anyValue The `Any` box.
+ @param anyValue The \c Any box.
  @return The underlying type metadata.
  */
 const void *_Nonnull trill_getAnyTypeMetadata(TRILL_ANY anyValue);
 
 
 /**
- Checks if the underlying metadata of an `Any` matches the metadata provided.
+ Checks if the underlying metadata of an \c Any matches the metadata provided.
 
- @param anyValue_ The `Any` whose type you're checking.
- @param typeMetadata_ The `TypeMetadata` you're checking.
- @return A non-zero value if the type metadata underlying the `Any` box
-         is pointer-equal to the provided `TypeMetadata`. Otherwise, 0.
+ @param anyValue_ The \c Any whose type you're checking.
+ @param typeMetadata_ The \c TypeMetadata you're checking.
+ @return A non-zero value if the type metadata underlying the \c Any box
+         is pointer-equal to the provided \c TypeMetadata. Otherwise, 0.
  */
 uint8_t trill_checkTypes(TRILL_ANY anyValue_,
                          const void *_Nonnull typeMetadata_);
 
 
 /**
- Checks if the underlying metadata of an `Any` box matches the provided
+ Checks if the underlying metadata of an \c Any box matches the provided
  metadata, and returns a pointer to the underlying payload if they do.
  
- @note If the `Any` value does not match the provided metadata, then this
+ @note If the \c Any value does not match the provided metadata, then this
        function causes a fatal error with a descriptive message and then
        aborts with a stack trace.
 
- @param anyValue_ The `Any` you're trying to cast.
- @param typeMetadata_ The `TypeMetadata` you're checking the `Any` against.
+ @param anyValue_ The \c Any you're trying to cast.
+ @param typeMetadata_ The \c TypeMetadata you're checking the \c Any against.
  @return A pointer to the payload that is safe to cast based on the type
          metadata.
  */
@@ -239,15 +239,15 @@ const void *_Nonnull trill_checkedCast(TRILL_ANY anyValue_,
 
 
 /**
- Determines if the value underlying an `Any` is `nil`. If the type underlying
- the `Any` is not a pointer or indirect type, then this will always return
- `false`. However, if the underlying value is a pointer or indirect type, then
+ Determines if the value underlying an \c Any is \c nil. If the type underlying
+ the \c Any is not a pointer or indirect type, then this will always return
+ \c false. However, if the underlying value is a pointer or indirect type, then
  this function will read the payload and see if the value in the payload is
- `NULL`.
+ \c NULL.
 
- @param any_ The `Any` you're checking.
+ @param any_ The \c Any you're checking.
  @return A non-zero value if the underlying payload should be interpreted as a
-         `nil` value.
+         \c nil value.
  */
 uint8_t trill_anyIsNil(TRILL_ANY any_);
 
